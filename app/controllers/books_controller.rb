92b6682696c64c8d8ac71632a9aebe7375be2e7a
github username <github email address>
2019-12-18 14:38:44 -0500

    fix patch

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,7 @@ class BooksController < ApplicationController
   end
 
   get '/books/new' do
+    #add if logged in
     erb :'/books/new'
   end
 
@@ -18,7 +19,7 @@ class BooksController < ApplicationController
       user = current_user
       book = user.books.build(params)
       if book.save
-        redirect "/books"
+        redirect "/books/:id"
       else
         redirect "/books/new"
       end
@@ -28,11 +29,8 @@ class BooksController < ApplicationController
   end
 
   get '/books/:id' do
-    # @book = Book.find_by(id: params[:id])
     if logged_in?
-      user = current_user
-      # book = user.books  
-      # book = user.books.build(params)
+      @book = current_user.books.find_by(id: params[:id])
       if @book
         erb :'books/show'
       else
@@ -43,24 +41,21 @@ class BooksController < ApplicationController
 
   get "/books/:id/edit" do
     if logged_in?
-      user = current_user
-      # book = user.books
-      # user = Book.find_by(id: params[:id]).user
-      if user == current_user
-        @users = User.all
-        @book = user.book
+      @book = current_user.books.find_by(id: params[:id])
+      if @book 
         erb :'/books/edit'
+      else
+       redirect '/books'
       end
+    else
+      redirect '/'
     end
   end
 
   patch "/books/:id" do
     if logged_in?
-      user = current_user
-      book = user.books
-    # user = Book.find_by(id: params[:id])
-      if user == current_user
-        @book = current_user
+      @book = current_user.books.find_by(id: params[:id])
+      if @book
         if @book.update(title: params[:title], author: params[:author], publisher: params[:publisher], genre: params[:genre])
           redirect "/books/#{@book.id}"
         else
