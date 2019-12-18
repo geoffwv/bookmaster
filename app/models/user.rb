8d17d720e07b6731f2fc876694d7f341c918ab6e
github username <github email address>
2019-12-18 11:33:32 -0500

    fixed index page code

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,5 @@ class User < ActiveRecord::Base
   has_many :books
   has_secure_password
   validates :email, presence: true
-  # validates :password, presence: true
   validates :email, uniqueness: true
 end
